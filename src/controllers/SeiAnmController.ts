6ad2cdf20feaaf62038ee96106ec4255cc74fb91
Clayton Menezes dos Santos <claytonmenezes@hotmail.com>
2024-01-05 20:08:26 -0300

    Comment out code related to SEI ANM.

--- src/controllers/SeiAnmController.ts
+++ src/controllers/SeiAnmController.ts
@@ -22,25 +22,22 @@ export class SeiAnmController {
       const nup = req.query.nup as string
       let sei = await this.db.buscaSei(client, nup)
       res.status(200).send(sei)
-      if (sei && sei.Link) {
-        const page = await this.metodosNavegador.navegar(browser, sei?.Link)
-        sei = await this.seiAnm.pegaSei(page)
-      } else {
-        const page = await this.metodosNavegador.navegar(browser, 'https://sei.anm.gov.br/sei/modulos/pesquisa/md_pesq_processo_pesquisar.php?acao_externa=protocolo_pesquisar&acao_origem_externa=protocolo_pesquisar&id_orgao_acesso_externo=0')
-        await this.seiAnm.pesquisaSei(page, nup)
-        sei = await this.seiAnm.pegaSei(page)
-      }
-      const processo = await this.db.buscaProcessoPorNup(client, nup)
-      if (sei && processo && processo.Id) sei = await this.db.insereSei(client, sei, processo?.Id)
-      axios({
-        baseURL: process.env.URL_SOCKET,
-        params: {sessionId: req.query.sessionId},
-        url: '/buscaSei',
-        data: sei
-      })
-    } catch (error) {
-      if (error instanceof Error) return res.status(400).send(error.message)
-      else return res.status(500).send('Internal Server Error')
+      // if (sei && sei.Link) {
+      //   const page = await this.metodosNavegador.navegar(browser, sei?.Link)
+      //   sei = await this.seiAnm.pegaSei(page)
+      // } else {
+      //   const page = await this.metodosNavegador.navegar(browser, 'https://sei.anm.gov.br/sei/modulos/pesquisa/md_pesq_processo_pesquisar.php?acao_externa=protocolo_pesquisar&acao_origem_externa=protocolo_pesquisar&id_orgao_acesso_externo=0')
+      //   await this.seiAnm.pesquisaSei(page, nup)
+      //   sei = await this.seiAnm.pegaSei(page)
+      // }
+      // const processo = await this.db.buscaProcessoPorNup(client, nup)
+      // if (sei && processo && processo.Id) sei = await this.db.insereSei(client, sei, processo?.Id)
+      // axios({
+      //   baseURL: process.env.URL_SOCKET,
+      //   params: {sessionId: req.query.sessionId},
+      //   url: '/buscaSei',
+      //   data: sei
+      // })
     } finally {
       this.db.desconectar(client)
       this.metodosNavegador.fecharBrowser(browser)
